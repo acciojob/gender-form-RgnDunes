73b113a30ed5208f1ef71f0161f4450a54e366f2
VIIBII <birlavishal90@gmail.com>
2023-09-08 02:03:15 +0530

    submitting code-Fri Sep 08 2023 02:03:14 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Gender form initialization</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
